6adcb18dd46e49afc64d49670a5eaffe6489c788
thornystefans <thorny17@ru.is>
2018-12-12 11:31:08 +0000

    refactored difficulty fragment

--- app/src/main/java/c/b/a/sudokuapp/MenuFragment.java
+++ app/src/main/java/c/b/a/sudokuapp/MenuFragment.java
@@ -6,7 +6,6 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
@@ -15,16 +14,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import com.facebook.AccessToken;
 import com.facebook.login.LoginManager;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -357,7 +352,7 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
         if(v == logout) {
             logout();
         }
-        // Start a new game
+        // Changes the screen so that the user can choose difficulty for the game
         else if(v == newGame) {
             newGame();
         }
